70822b0d2fc4e8fc7e25d93fe71b8b3c543bee1e
mithu0621 <mithunisk03@gmail.com>
2025-01-17 10:14:56 +0530

    first commit

---
+++ demo/main.py
@@ -0,0 +1,8 @@
+name = "Json"
+
+if len(name) > 50:
+   print("Cannot include more than 50 characters!")
+elif len(name) <3:
+    print("Password must contain at-least 3 characters!")
+else:
+    print("Password looks good!")
